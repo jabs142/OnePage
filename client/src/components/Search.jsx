3875655fc2269d228458240a38876748beadc9cc
Jabs <98640151+JT142@users.noreply.github.com>
2022-11-18 14:52:12 +0800

    Created a search bar. No functionality yet

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import NavBar from './NavBar';
+import SearchBar from './SearchBar';
 
 function Search() {
     return (
         <div>
             <NavBar />
             <h1> Search function here </h1>
+            <SearchBar />
         </div>
     )
 }
---
+++ client/src/components/SearchBar.jsx
@@ -0,0 +1,19 @@
+// import React, { useState, useEffect } from "react";
+import { FaSearch } from "react-icons/fa";
+
+function SearchBar() {
+    return (
+        <div className="search-form">
+            <form className="search-form">
+                <input className="shadow appearance-none border rounded w-100 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Enter book title"></input>
+    
+                <button type="submit" className="search-button p-4">
+                    <FaSearch size={20}/>
+                </button>
+            </form>
+        </div>
+    )
+}
+
+export default SearchBar
+
